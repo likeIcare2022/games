99b5cf0f13f4a851560b772cc873995fd7b5cf8a
G1aD05 <ezrag2000@proton.me>
2026-03-29 18:05:06 -0700

    updated proxy link

---
+++ tools/tools/scramjet.mercurywork.shop/cdn.jsdelivr.net/sm/jsdelivr-header.js
@@ -0,0 +1,6 @@
+/**
+ * Minified by jsDelivr using Terser v5.37.0.
+ * Original file: /npm/dreamland@0.0.25/dist/dev.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
